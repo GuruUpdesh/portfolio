fbb4a1290b51f80c00fc6c4d73dbfddb038584b2
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-07-13 21:10:09 -0700

    added analytics to portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const sora = Sora({ subsets: ["latin"] });
@@ -17,6 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={sora.className}>{children}</body>
+            <Analytics />
         </html>
     );
 }
