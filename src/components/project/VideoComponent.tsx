5119f7f25f6dfebe99f55adacd5049ef673956df
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-07-26 19:25:00 -0700

    switched to cloudinary for video hosting:

--- src/components/project/VideoComponent.tsx
+++ src/components/project/VideoComponent.tsx
@@ -4,16 +4,16 @@ import { list } from "@vercel/blob";
 import VideoPlayer from "./VideoPlayer";
 
 async function VideoComponent({ filename }: { filename: string }) {
-    const { blobs } = await list({
-        prefix: `Portfolio/${filename}`,
-        limit: 1,
-    });
+    // const { blobs } = await list({
+    //     prefix: `Portfolio/${filename}`,
+    //     limit: 1,
+    // });
 
-    console.log(blobs);
+    // console.log(blobs);
 
-    const { url } = blobs[0];
+    // const { url } = blobs[0];
 
-    return <VideoPlayer url={url} />;
+    return <VideoPlayer url={filename} />;
 }
 
 export default VideoComponent;
