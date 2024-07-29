803295aaeafb856e6b5be25954435f5d65906389
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-07-28 22:22:36 -0700

    polished project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,18 +16,17 @@ const ProjectCard = ({ project, className }: Props) => {
         <Link
             href={`/projects/${project.key}`}
             className={cn(
-                "group relative z-10 min-h-[200px] overflow-clip rounded-sm bg-[#fafafa] text-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring dark:bg-[#0A0A0A]",
+                "group z-10 min-h-[200px] overflow-clip rounded-sm bg-[#fafafa] text-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring dark:bg-[#0A0A0A]",
                 className,
             )}
         >
-            <div className="absolute bottom-0 left-0 z-0 h-20 w-full bg-gradient-to-b from-transparent to-background"></div>
             <div className="flex h-full flex-col overflow-hidden rounded-md transition-all group-focus-visible:bg-border/10 group-focus-visible:ring-1 group-focus-visible:ring-ring/25 md:group-active:scale-[97.5%]">
                 <div className="relative flex-1">
                     <Image
                         src={`/icons/${project.key}.ico`}
                         fill
                         alt="website icon"
-                        className="aspect-square opacity-[1%] blur-[100px] transition-opacity duration-1000 ease-out-expo group-hover:opacity-25 group-focus-visible:opacity-25"
+                        className="aspect-square translate-y-[-50%] opacity-[1%] blur-[100px] transition-opacity duration-1000 ease-out-expo group-hover:opacity-25 group-focus-visible:opacity-25"
                     />
                     <Image
                         src={`/${project.key}.webp`}
@@ -37,25 +36,20 @@ const ProjectCard = ({ project, className }: Props) => {
                         quality={100}
                     />
                 </div>
-                <div className="z-10 h-5 overflow-hidden pl-2 text-muted-foreground">
-                    <div className="transition-transform duration-500 ease-out-expo group-hover:-translate-y-6 group-focus-visible:-translate-y-6">
-                        <p>{project.year}</p>
+                <div className="h-5 overflow-hidden pl-2 text-muted-foreground">
+                    <div className="-translate-y-6 transition-transform duration-500 ease-out-expo group-hover:translate-y-0 group-focus-visible:translate-y-0">
                         <PrimaryTech title={project.primaryTech} />
+                        <p>{project.year}</p>
                     </div>
                 </div>
-                <div className="z-10 flex w-full items-center justify-between p-2 pt-0 transition-all ease-out group-hover:pr-1 group-focus-visible:pr-1">
+                <div className="flex w-full items-center justify-between p-2 pt-0 transition-all ease-out group-hover:pr-1 group-focus-visible:pr-1">
                     <p className="flex items-center gap-2 truncate text-lg text-muted-foreground transition-colors group-hover:text-primary group-focus-visible:text-primary">
-                        <div className="h-4 overflow-hidden">
-                            <div className="transition-transform duration-500 -translate-y-4 ease-out-expo group-hover:translate-y-0 group-focus-visible:translate-y-0">
-                                <Eye className="h-4 w-4 opacity-50 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100" />
-                                <Image
-                                    src={`/icons/${project.key}.ico`}
-                                    height={16}
-                                    width={16}
-                                    alt="website icon"
-                                />
-                            </div>
-                        </div>
+                        <Image
+                            src={`/icons/${project.key}.ico`}
+                            height={16}
+                            width={16}
+                            alt="website icon"
+                        />
                         {project.name}
                     </p>
                     <ArrowRight className="h-4 w-4 opacity-50 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100" />
