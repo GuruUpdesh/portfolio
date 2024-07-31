339ab636160e0c4ed4a60e62e7027b2a7e573db8
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-07-31 11:50:57 -0700

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/lib/ThemeProvider";
@@ -35,6 +36,7 @@ export default function RootLayout({
                 </ThemeProvider>
             </body>
             <Analytics />
+            <SpeedInsights />
         </html>
     );
 }
