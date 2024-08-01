74c63c2cc7eeebfaba3264df1ed985655128af7f
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-07-31 20:03:42 -0700

    simplified contact info

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,14 +1,4 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import {
-    ArrowTopRightIcon,
-    ChatBubbleIcon,
-    GitHubLogoIcon,
-    LinkedInLogoIcon,
-} from "@radix-ui/react-icons";
-import { FaDiscord } from "react-icons/fa";
-import { Download } from "lucide-react";
 
 const ContactInfo = () => {
     return (
@@ -19,77 +9,12 @@ const ContactInfo = () => {
                 connect and explore how we can create something great together.
             </p>
             <div>
-                <h3>Email me</h3>
+                <p>Email me</p>
                 <p className="text-sm font-light text-muted-foreground">
                     guruupdeshsingh@gmail.com
                 </p>
             </div>
-            <div>
-                <h3>Work experience</h3>
-                <Link
-                    href="/Resume.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Download Resume"
-                    className="flex items-center gap-2 text-sm font-light text-muted-foreground transition-colors hover:text-primary"
-                >
-                    <Download className="h-4 w-4" />
-                    Resume
-                </Link>
-            </div>
-            <div>
-                <h3 className="mb-2">Follow me</h3>
-                <ul className="flex flex-col gap-2" aria-label="Social links">
-                    <li>
-                        <SocialLink
-                            name="GitHub"
-                            link="https://github.com/GuruUpdesh"
-                        >
-                            <GitHubLogoIcon className="h-4 w-4" />
-                        </SocialLink>
-                    </li>
-                    <li>
-                        <SocialLink
-                            name="LinkedIn"
-                            link="https://www.linkedin.com/in/guru-updesh-singh-789050218"
-                        >
-                            <LinkedInLogoIcon className="h-4 w-4" />
-                        </SocialLink>
-                    </li>
-                    <li>
-                        <SocialLink
-                            name="Discord"
-                            link="https://discord.com/channels/.gsingh"
-                        >
-                            <FaDiscord className="h-4 w-4" />
-                        </SocialLink>
-                    </li>
-                </ul>
-            </div>
         </div>
-    );
-};
-
-type Link = {
-    name: string;
-    link: string;
-    children: React.ReactNode;
-};
-
-const SocialLink = ({ name, link, children }: Link) => {
-    return (
-        <Link
-            href={link}
-            target="_blank"
-            className="group flex items-center gap-2 text-sm font-light text-muted-foreground transition-all hover:text-primary"
-            aria-label={name}
-        >
-            <span className="flex items-center gap-2 transition-[padding] group-hover:pl-1">
-                {children}
-                {name}
-            </span>
-            <ArrowTopRightIcon className="scale-0 opacity-0 transition-all ease-out-expo group-hover:scale-100 group-hover:opacity-100" />
-        </Link>
     );
 };
 
