922fbb309dbd50ef167d387a8f664550ac0692d6
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-08-02 15:57:37 -0700

    rounded corners on image gallery

--- src/components/project/Gallery.tsx
+++ src/components/project/Gallery.tsx
@@ -25,19 +25,26 @@ const Gallery = ({ images, projectKey }: Props) => {
         <CursorTracker className="h-full w-full">
             <div
                 ref={gridRef}
-                className="group/grid relative mt-[-1px] grid w-full grid-cols-2 gap-[1px] overflow-hidden bg-border p-[1px] xl:grid-cols-4"
+                className="group/grid relative mt-[-1px] grid w-full grid-cols-2 gap-[1px] overflow-hidden bg-border p-[1px] xl:grid-cols-4 xl:rounded-[40px]"
             >
                 <div className="pointer-events-none absolute left-[var(--x)] top-[var(--y)] h-[300px] w-[300px] translate-x-[-50%] translate-y-[-50%] bg-primary opacity-0 blur-3xl transition-opacity duration-300 ease-out-expo md:group-hover/grid:opacity-100" />
                 {images.map(({ src, alt }, index) => (
                     <div
                         key={index}
-                        className="group/image relative aspect-[6/7] overflow-hidden bg-background transition-all"
+                        className={cn(
+                            "gallery-image group relative aspect-[6/7] overflow-hidden bg-background transition-all",
+                            {
+                                "xl:rounded-l-[40px]": index === 0,
+                                "xl:rounded-r-[40px]":
+                                    index === images.length - 1,
+                            },
+                        )}
                     >
                         <Image
                             src={getImage(projectKey, src)}
                             fill
                             className={cn(
-                                "object-cover object-left-top transition-all duration-500 ease-out-expo group-hover/grid:opacity-50 group-hover/image:!opacity-100 group-hover/grid:blur-[2px] group-hover/image:!blur-0",
+                                "object-cover object-left-top transition-all duration-1000 group-hover/grid:opacity-25 group-hover:!opacity-100 group-hover/grid:ease-out-expo",
                                 { "opacity-0": !isInView },
                             )}
                             priority
