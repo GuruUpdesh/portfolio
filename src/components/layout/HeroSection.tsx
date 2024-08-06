41fd001f73c1eb2908605d9147c31dfabd143de6
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-08-06 15:57:41 -0700

    added fallback css property

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -5,7 +5,7 @@ import SizeTracker from "../SizeTracker";
 const HeroSection = () => {
     return (
         <div className="relative">
-            <SizeTracker className="relative z-10 overflow-clip rounded-br-[var(--height)] border border-l-0 md:border-l border-t-0 bg-background pt-[74px] md:rounded-bl-[60px] md:pt-0">
+            <SizeTracker className="relative z-10 overflow-clip rounded-br-[var(--height),440px] border border-l-0 border-t-0 bg-background pt-[74px] transition-all md:rounded-bl-[60px] md:border-l md:pt-0">
                 <div className="flex h-full w-full flex-col justify-between gap-10 px-5 pb-[120px] pt-[60px] sm:px-[60px] md:flex-row lg:gap-0">
                     <div className="flex flex-col justify-between">
                         <p className="mb-6 text-xl transition-[margin] lg:text-2xl">
@@ -25,7 +25,7 @@ const HeroSection = () => {
                     </p>
                 </div>
                 <div className="absolute top-0 -z-10 h-full w-full overflow-clip blur-[100px]">
-                    <div className="absolute top-[25%] w-[200%] md:w-[100%] -hue-rotate-30 h-[200%] md:right-[-50%] md:top-[-50%]">
+                    <div className="absolute top-[25%] h-[200%] w-[200%] -hue-rotate-30 md:right-[-50%] md:top-[-50%] md:w-[100%]">
                         <Image
                             fill
                             src="/logo.webp"
