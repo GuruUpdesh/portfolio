1e299cbc0a4a6debe18b79f0548b9fa330f71031
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-08-06 16:18:24 -0700

    fixed bug with border raidus in chrome

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -5,7 +5,7 @@ import SizeTracker from "../SizeTracker";
 const HeroSection = () => {
     return (
         <div className="relative">
-            <SizeTracker className="relative z-10 overflow-clip rounded-br-[var(--height,440px)] border border-l-0 border-t-0 bg-background pt-[74px] transition-all md:rounded-bl-[60px] md:border-l md:pt-0">
+            <SizeTracker className="relative z-10 overflow-clip rounded-br-[calc(var(--height,440px)-1px)] border border-l-0 border-t-0 bg-background pt-[74px] transition-all md:rounded-bl-[60px] md:border-l md:pt-0">
                 <div className="flex h-full w-full flex-col justify-between gap-10 px-5 pb-[120px] pt-[60px] sm:px-[60px] md:flex-row lg:gap-0">
                     <div className="flex flex-col justify-between">
                         <p className="mb-6 text-xl transition-[margin] lg:text-2xl">
